cd4ebaede098b76ad8bea2dad1f0929cb7d19115
Ziad-Elsaed <ziadelsaed1@gmail.com>
2026-06-21 21:15:55 +0300

    testing by removing about me section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,6 +35,14 @@ const services = [
         icon: Camera,
         title: "Account & Production Management",
     },
+    {
+        icon: Camera,
+        title: "Account & Production Management",
+    },
+    {
+        icon: Camera,
+        title: "Account & Production Management",
+    },
 ];
 
 export default function AboutMe() {
